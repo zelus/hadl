ba2482d316fcae1c13ee72580bbef2869f52e9fa
zelus <gwendal.daniel@gmail.com>
2013-10-25 16:22:47 +0200

    factorise checkPorts in ComponentService also separate req and prov ports

--- src/M2/ComponentService.java
+++ src/M2/ComponentService.java
@@ -9,25 +9,45 @@ import M2.exceptions.ComponentServiceException;
 
 public abstract class ComponentService extends ComponentInterface {
 
-	private ArrayList<ComponentPort> usedPorts;
+	private ArrayList<ComponentPort> reqPorts;
+	private ArrayList<ComponentPort> provPorts;
 	
 	/**
 	 * Create a new service associated to the given Component.
 	 * 
 	 * @param name the name of the service.
 	 * @param parent the Component the service belongs to.
-	 * @param usedPorts the ports (provided and required) used by the service.
+	 * @param reqPorts the required ports used by the service.
+	 * @param provPorts the provided ports used by the service.
 	 * @throws ComponentServiceException when at least one port doesn't belong to 
 	 * the same parent component.
 	 */
-	public ComponentService(String name, Component parent, ComponentPort[] usedPorts) throws ComponentServiceException {
+	public ComponentService(String name, Component parent, ComponentPort[] reqPorts, ComponentPort[] provPorts) throws ComponentServiceException {
 		super(name,parent);
 		/*
 		 * Check if all the given ports belong to the same
 		 * parent as the current service
 		 */
-		this.usedPorts = new ArrayList<ComponentPort>(Arrays.asList(usedPorts));
-		Iterator<ComponentPort> it = this.usedPorts.iterator();
+		this.reqPorts = new ArrayList<ComponentPort>(Arrays.asList(reqPorts));
+		this.provPorts = new ArrayList<ComponentPort>(Arrays.asList(provPorts));
+		checkPorts(this.reqPorts);
+		checkPorts(this.provPorts);
+	}
+	
+	public final Object call() {
+		/*
+		 * Prepare the required ports by calling their
+		 * prepareValue method. The runtime will process the
+		 * request and ensure all the reqPorts have their value accessible.
+		 */
+		return new Object();
+	}
+	
+	protected abstract Object run(Collection<ComponentPort> reqPorts, Collection<ComponentPort> provPorts);
+	
+	
+	private void checkPorts(Collection<ComponentPort> ports) throws ComponentServiceException {
+		Iterator<ComponentPort> it = ports.iterator();
 		while(it.hasNext()) {
 			String portParentName = it.next().getParent().getName();
 			if(!portParentName.equals(this.getParent().getName())) {
@@ -35,12 +55,5 @@ public abstract class ComponentService extends ComponentInterface {
 			}
 		}
 	}
-	
-	public final Object call() {
-		// TODO get the values of the reqPorts
-		return new Object();
-	}
-	
-	protected abstract Object run(Collection<ComponentPort> reqPorts, Collection<ComponentPort> provPorts);
 	
 }
